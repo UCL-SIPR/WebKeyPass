bd75f65088628c109c90158bbc0c992d37bab5e6
Sébastien Wilmet <swilmet@gnome.org>
2013-04-11 15:25:29 +0200

    Login page: link to create a new account

---
+++ Controller/CreateAccountController.php
@@ -0,0 +1,49 @@
+<?php
+/*
+ * This file is part of WebKeyPass.
+ *
+ * Copyright © 2013 Université Catholique de Louvain
+ *
+ * WebKeyPass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * WebKeyPass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with WebKeyPass.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author: Sébastien Wilmet
+ */
+
+namespace UCL\WebKeyPassBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use UCL\WebKeyPassBundle\Entity\User;
+
+class CreateAccountController extends MainController
+{
+    public function getCommonData ()
+    {
+        $data = array ();
+        $data['title'] = 'Create Account';
+        return $data;
+    }
+
+    public function createAccountAction ()
+    {
+        $user = new User ();
+        $user->setIsActive (false);
+        $user->setIsAdmin (false);
+
+        $action = new CreateUserAction ($this, $user);
+        $action->setRedirectRoute ('ucl_wkp_login');
+
+        return $action->handleForm ();
+    }
+}
